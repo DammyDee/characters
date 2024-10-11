2e852ae923ca5777d09104413311f38ec4a72435
DammyDee <damola.adewunmi@gmail.com>
2024-10-11 15:44:51 +0100

    push

---
+++ characters/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace characters;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "6,889,899.45";
+        Console.WriteLine(IsDouble(s));
+        Console.WriteLine(Convert.ToDouble(s));
+    }
+
+    //Convert string to a double
+    static bool IsDouble(string s)
+    {   
+        try{
+            Convert.ToDouble(s);
+        }
+        catch(FormatException){
+            return false;
+        }
+        catch(OverflowException){
+            return false;
+        }
+        return true;
+    }
+}
